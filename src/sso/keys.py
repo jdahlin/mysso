4b199225559da0ef766194e90e8470b0dd5f9862
Johan Dahlin <jdahlin@gmail.com>
2023-03-28 17:16:40 +0200

    Fix loading of generated keys after move to src

--- src/sso/keys.py
+++ src/sso/keys.py
@@ -1,17 +1,20 @@
 """This module contains public and private Jwk keys."""
 from functools import lru_cache
+from pathlib import Path
 
 from jwskate import Jwk
 
 from sso.settings import JWT_ALGORITHM
 
+key_dir = Path(__file__).parent.parent.parent / "keys"
+
 
 @lru_cache
 def get_private_key() -> Jwk:
     """Return the private key used to sign JWTs."""
-    with open("keys/private_key.pem") as f:
+    with (key_dir / "private_key.pem").open() as f:
         private_key = Jwk.from_pem_key(f.read())
-        if private_key.is_private:
+        if not private_key.is_private:
             raise AssertionError("Private key is not private")
         return private_key
 
@@ -19,8 +22,8 @@ def get_private_key() -> Jwk:
 @lru_cache
 def get_public_key() -> Jwk:
     """Return the public key used to verify JWTs."""
-    with open("keys/public_key.pem") as f:
-        pubic_key = Jwk.from_pem_key(f.read())
-        if not pubic_key.is_private:
+    with (key_dir / "public_key.pem").open() as f:
+        public_key = Jwk.from_pem_key(f.read())
+        if public_key.is_private:
             raise AssertionError("Public key is not public")
-        return pubic_key.with_usage_parameters(alg=JWT_ALGORITHM, with_use=True)
+        return public_key.with_usage_parameters(alg=JWT_ALGORITHM, with_use=True)
